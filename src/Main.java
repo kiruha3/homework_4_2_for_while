d835e95d8eb190082bd98d3bb5e6ce5ee6d5ac12
kirill <kiruha3@mail.ru>
2023-06-15 10:50:13 +0700

    1,2,пол3, задачи

--- src/Main.java
+++ src/Main.java
@@ -15,16 +15,44 @@ public class Main {
 
     private static void task1() {
         System.out.println("Задача 1");
-
+        int nowVklad = 0;
+        int moneyPerMonth = 15_000;
+        int allSum = 2_459_000;
+        int i = 0;
+        while (allSum > nowVklad) {
+            i++;
+            nowVklad += moneyPerMonth;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + nowVklad + " рублей");
+        }
     }
 
     private static void task2() {
         System.out.println("Задача 2");
+        int i = 0;
+        while (i <= 10) {
+            System.out.printf(i++ + " ");
+        }
+        System.out.println();
 
+        for (int j = 10; j >= 1; j--) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
     }
 
     private static void task3() {
         System.out.println("Задача 3");
+        int humanY = 12_000_000;
+        int souls =
+        int fertilityPerYearOnThousand = 17;
+        int diePerYearOnThousand = 8;
+        int fertPerYear = 0;
+        int diePerYear = 0;
+        for (int i = 0; i <= 10; i++) {
+            fertPerYear = humanY / 1000 * fertilityPerYearOnThousand;
+            diePerYear = humanY / 1000 * diePerYearOnThousand;
+            humanY=humanY+fertPerYear-diePerYear;
+        }
 
     }
 
@@ -56,6 +84,7 @@ public class Main {
         System.out.println("Задача 9");
 
     }
+
     private static void task10() {
 
     }

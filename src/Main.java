d28af6ec01a9576ef3a7c4456cf134f519033461
kirill <kiruha3@mail.ru>
2023-06-15 10:22:58 +0700

    firt_commit

---
+++ src/Main.java
@@ -0,0 +1,62 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+        task9();
+        task10();
+
+    }
+
+    private static void task1() {
+        System.out.println("Задача 1");
+
+    }
+
+    private static void task2() {
+        System.out.println("Задача 2");
+
+    }
+
+    private static void task3() {
+        System.out.println("Задача 3");
+
+    }
+
+    private static void task4() {
+        System.out.println("Задача 4");
+
+    }
+
+    private static void task5() {
+        System.out.println("Задача 5");
+
+    }
+
+    private static void task6() {
+        System.out.println("Задача 6");
+    }
+
+    private static void task7() {
+        System.out.println("Задача 7");
+
+    }
+
+    private static void task8() {
+        System.out.println("Задача 8");
+
+    }
+
+    private static void task9() {
+        System.out.println("Задача 9");
+
+    }
+    private static void task10() {
+
+    }
+}
